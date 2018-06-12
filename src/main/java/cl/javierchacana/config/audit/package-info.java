4eb2a2960e472b272e4783f60f34779679272b89
Javier Chacana <javierchacana@Javiers-MacBook-Pro.local>
2018-06-12 14:39:09 +0800

    Initial application generated by JHipster-4.14.4

---
+++ src/main/java/cl/javierchacana/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package cl.javierchacana.config.audit;
---
+++ src/test/java/cl/javierchacana/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,18 @@
+package cl.javierchacana.cucumber.stepdefs;
+
+import cl.javierchacana.ConferenceApp;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@WebAppConfiguration
+@SpringBootTest
+@ContextConfiguration(classes = ConferenceApp.class)
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
